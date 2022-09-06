c8596ec6e4901b052ee0de71f16935fe9eb81787
PARKSANGHYUN3 <basira@easymedia.net>
2022-09-06 18:06:49 +0900

    집에서 할거

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-  var h = $(".info_wrap").innerHeight();
-  console.log(h);
+  var h = $(".info_wrap").outerHeight();
+  var h2 = $(".history_wrap").outerHeight();
+  var h3 = $(".info_wrap").outerHeight();
+  var h4 = $(".scroll_wrap").outerHeight();
+  var height = h3 + h4;
   var tl = gsap.timeline();
-  var progress = tl.progress(); 
   tl.to(".scroll_wrap", {
     scrollTrigger: {
-      trigger: ".info_wrap",
-      start: "-80% top",
-      end: "90% bottom",
+      trigger: ".scroll_wrap",
+      start: "-30% top",
+      end: height + " 30%",
       pin: ".scroll_wrap",
       pinSpacing: false,
       markers: true,
@@ -21,22 +23,22 @@ $(document).ready(function(){
         }
 
         console.log(self.progress.toFixed(3))
-        if(self.progress.toFixed(3) > 0.4) {
+        if(self.progress.toFixed(3) > 0.2) {
           console.log("지금!");
           $(".heritage_conlist").addClass("onScroll");
-
         } else {
           $(".heritage_conlist").removeClass("onScroll");
         }
 
-        if(self.progress.toFixed(3) > 0.45) {
+        if(self.progress.toFixed(3) > 0.35) {
           $(".heritage_conlist").addClass("active");
           console.log("나온다!");
+        } else {
+          $(".heritage_conlist").removeClass("active");
         }
+
       }
     }
   })
-  // .to(".heritage_inner", {
-  //   width: "1200px"
-  // });
+  
 });
