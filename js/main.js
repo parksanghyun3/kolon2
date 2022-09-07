54df9fb683d369ca2d7e27e25d056aea6c31db3b
PARKSANGHYUN3 <basira@easymedia.net>
2022-09-07 17:55:49 +0900

    피드백 수정중

--- js/main.js
+++ js/main.js
@@ -1,44 +1,63 @@
 $(document).ready(function(){
   var h = $(".heritage_conlist").outerHeight();
-
   var tl = gsap.timeline();
-
-
-  tl.to(".scroll_wrap", {
-    scrollTrigger: {
-      trigger: ".heritage_inner",
-      start: "top top",
-      end: "bottom bottom",
-      endTrigger: ".heritage_conlist",
-      pin: ".scroll_wrap",
-      pinSpacing: false,
-      markers: true,
-      onUpdate(self){
-        if(self.progress.toFixed(3) == 0) {
-          $(".heritage_conlist").removeClass("onBg");
+  // var txtH = $(".history_wrap").outerHeight();
+  $(".heritage_conlist").each(function(i){
+    tl.to($(".heritage_conlist").find(".scroll_wrap"), {
+      scrollTrigger: {
+        trigger: $(".heritage_conlist").eq(i).find(".heritage_inner"),
+        start: "top top",
+        end: "bottom center",
+        endTrigger: $(".heritage_conlist").eq(i).find(".history_wrap"),
+        pin: $(".heritage_conlist").eq(i).find(".scroll_wrap"),
+        pinSpacing: false,
+        markers: true,
+        onUpdate(self){
+          if(self.progress.toFixed(3) == 0) {
+            $(".heritage_conlist").eq(i).removeClass("onBg");
+          }
+  
+          if(self.progress.toFixed(3) > 0.003) { 
+            $(".heritage_conlist").eq(i).addClass("onBg");
+          }
+  
+          console.log(self.progress.toFixed(3))
+          if(self.progress.toFixed(3) > 0.2) {
+            // console.log("지금!");
+            $(".heritage_conlist").eq(i).addClass("onScroll");
+          } else {
+            $(".heritage_conlist").eq(i).removeClass("onScroll");
+          }
+  
+          if(self.progress.toFixed(3) > 0.35) {
+            $(".heritage_conlist").eq(i).addClass("active");
+            // console.log("나온다!");
+          } else {
+            $(".heritage_conlist").eq(i).removeClass("active");
+          }
+  
         }
-
-        if(self.progress.toFixed(3) > 0.003) { 
-          $(".heritage_conlist").addClass("onBg");
-        }
-
-        console.log(self.progress.toFixed(3))
-        if(self.progress.toFixed(3) > 0.2) {
-          console.log("지금!");
-          $(".heritage_conlist").addClass("onScroll");
-        } else {
-          $(".heritage_conlist").removeClass("onScroll");
-        }
-
-        if(self.progress.toFixed(3) > 0.35) {
-          $(".heritage_conlist").addClass("active");
-          console.log("나온다!");
-        } else {
-          $(".heritage_conlist").removeClass("active");
-        }
-
       }
-    }
+    })
+
+    $(".heritage_conlist").eq(i).find(".heritage_img_list").each(function(e){
+      $(window).on("scroll", function(){
+        var imgSct = $(".heritage_conlist").eq(i).find(".scroll_wrap").offset().top;
+        var txtSct = $(".heritage_conlist").eq(i).find(".plus_img").eq(e).offset().top;
+        // var txtSct2 = $(".heritage_conlist").eq(i+1).find(".history_list").eq(e).offset().top;
+  
+        if(imgSct + 180 > txtSct) {
+          $(".heritage_conlist").eq(i).find(".heritage_img_list").eq(e).fadeIn();
+          // $(".heritage_conlist").eq(i).find(".history_list").eq(e).addClass("on")
+        } else {
+          $(".heritage_conlist").eq(i).find(".heritage_img_list").eq(e).fadeOut();
+          // $(".heritage_conlist").eq(i).find(".history_list").eq(e).removeClass("on");
+        }
+      });
+    });
+
   })
+
+
   
 });
